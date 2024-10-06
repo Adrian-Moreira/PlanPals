f346d04b058f4fed3be9e0c5a27c05464e94d2b9
unknown <cruzra1@myumanitoba.ca>
2024-10-05 22:30:57 -0500

    implement travel form screen on android

---
+++ front-end-android/planpals/lib/features/travel_planner/views/travel_planner_view.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:planpals/core/components/error_message_screen.dart';
+import 'package:planpals/features/travel_planner/models/travel_planner_model.dart';
+import 'package:planpals/core/constants.dart';
+import 'components/listviews/accommodation_list_view.dart';
+import 'components/listviews/activity_list_view.dart';
+import 'components/listviews/flight_list_view.dart';
+
+
+class TravelPlannerView extends StatelessWidget {
+  final TravelPlanner travelPlanner;
+  const TravelPlannerView({super.key, required this.travelPlanner});
+
+  @override
+  Widget build(BuildContext context) {
+    final travelPlanner = context;
+
+    return 
+      // check if travelPlanner is null
+      // ignore: unnecessary_null_comparison
+      travelPlanner == null
+        // if null, display error message for null travel planner
+        ? const ErrorMessageScreen(errorMessage: ErrorMessage.nullTravelPlanner, appBarTitle: '',)
+
+        // else display travel planner details
+        : _buildTravelPlanner(travelPlanner as TravelPlanner);
+  }
+
+  Widget _buildTravelPlanner(TravelPlanner travelPlanner) {
+    
+    return 
+      Scaffold(
+        appBar: AppBar(
+          title: Text(travelPlanner.plannerName),
+        ),
+
+        body:
+          CustomScrollView(
+            slivers: [
+          
+              // Sliver app bar
+              SliverAppBar(
+                expandedHeight: 200.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text(travelPlanner.destination),
+                ),
+                backgroundColor: const Color.fromRGBO(122, 22, 124, 1.0),
+              ),
+              
+          
+              SliverToBoxAdapter(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+          
+                      const SizedBox(height: 20,),
+          
+                      FlightListView(
+                        flightList: travelPlanner.flights,
+                        onAdd: () {
+                          return;
+                        },
+                      ),
+                
+                      const SizedBox(height: 20,),
+                      const Divider(height: 1),
+                      const SizedBox(height: 10,),
+                
+                      AccommodationListView(
+                        accommodationList: travelPlanner.accommodations,
+                        onAdd: () {
+                          return;
+                        },
+                      ),
+                
+                      const SizedBox(height: 20,),
+                      const Divider(height: 1),
+                      const SizedBox(height: 10,),
+          
+                      ActivityListView(
+                        activityList: travelPlanner.activities,
+                        onAdd: () {
+                          return;
+                        },
+                      ),
+          
+                      const SizedBox(height: 20,),
+                    ],
+                  ),
+                ),
+              ), 
+            ],
+          ),
+      );
+  }
+}
+
