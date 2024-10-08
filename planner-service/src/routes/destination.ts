468a2a76a6976693144d93b11e90d4b9f062d682
h-kt <lammsuen@icloud.com>
2024-10-08 13:34:59 -0500

    Added destination routes

--- planner-service/src/routes/destination.ts
+++ planner-service/src/routes/destination.ts
@@ -1,91 +1,22 @@
-import express, { Request, Response, NextFunction } from "express"; // import { NextFunction } from "express";
-import { StatusCodes } from "http-status-codes";
-import { activityRouter } from "./activity";
-import { accommodationRouter } from "./accommodation";
+import express from 'express'
+import { activityRouter } from './activity'
+import { accommodationRouter } from './accommodation'
+import {
+  createDestination,
+  getDestinationsByPlannerId,
+  getDestinationById,
+  updateDestination,
+  deleteDestination,
+} from '../controllers/destinationController'
 
-// import {
-//   createDestination,
-//   getDestinationsByPlannerId,
-//   getDestinationById,
-//   updateDestinationById,
-//   deleteDestinationById,
-// } from '../controllers/destination';
+export const destinationRouter = express.Router({ mergeParams: true })
 
-export const destinationRouter = express.Router({ mergeParams: true });
+destinationRouter.get('/', getDestinationsByPlannerId)
+destinationRouter.post('/', createDestination)
 
-destinationRouter.get(
-  '/',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { plannerId } = req.params;
-    //   const result = await getDestinationsByPlannerId(plannerId);
-    //   res.status(StatusCodes.OK).json({ success: true, data: result });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-destinationRouter.post(
-  '/',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { plannerId } = req.params;
-      const destinationData = req.body;
-    //   const result = await createDestination(plannerId, destinationData);
-    //   res.status(StatusCodes.CREATED).json({ success: true, data: result });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-destinationRouter.get(
-  '/:destinationId',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { plannerId, destinationId } = req.params;
-    //   const result = await getDestinationById(plannerId, destinationId);
-    //   res.status(StatusCodes.OK).json({ success: true, data: result });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-destinationRouter.put(
-  '/:destinationId',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { plannerId, destinationId } = req.params;
-      const destinationData = req.body;
-    //   const result = await updateDestinationById(
-    //     plannerId,
-    //     destinationId,
-    //     destinationData,
-    //   );
-    //   res.status(StatusCodes.OK).json({ success: true, data: result });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-destinationRouter.delete(
-  '/:destinationId',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { plannerId, destinationId } = req.params;
-    //   await deleteDestinationById(plannerId, destinationId);
-    //   res
-    //     .status(StatusCodes.OK)
-    //     .json({ success: true, message: 'Destination deleted successfully.' });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
+destinationRouter.get('/:destinationId', getDestinationById)
+destinationRouter.patch('/:destinationId', updateDestination)
+destinationRouter.delete('/:destinationId', deleteDestination)
 
 destinationRouter.use('/:destinationId/accommodation', accommodationRouter)
 destinationRouter.use('/:destinationId/activity', activityRouter)
