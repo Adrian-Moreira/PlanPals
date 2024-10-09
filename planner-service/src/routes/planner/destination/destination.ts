022ab9b69e1f700b59cade0275fa58fd41b8272f
h-kt <lammsuen@icloud.com>
2024-10-09 15:53:04 -0500

    Removed untested files

--- planner-service/src/routes/planner/destination/destination.ts
+++ planner-service/src/routes/planner/destination/destination.ts
@@ -1,6 +1,4 @@
 import express from 'express'
-import { activityRouter } from './activity/activity'
-import { accommodationRouter } from './accommodation'
 import {
   createDestination,
   getDestinationsByPlannerId,
@@ -17,6 +15,3 @@ destinationRouter.post('/', createDestination)
 destinationRouter.get('/:destinationId', getDestinationById)
 destinationRouter.patch('/:destinationId', updateDestination)
 destinationRouter.delete('/:destinationId', deleteDestination)
-
-destinationRouter.use('/:destinationId/accommodation', accommodationRouter)
-destinationRouter.use('/:destinationId/activity', activityRouter)
