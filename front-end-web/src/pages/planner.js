7d51f87b471ac7d81480fb7c74b4f2205ff81e2a
JoshuaWolowich <120571945+JoshuaWolowich@users.noreply.github.com>
2024-10-09 15:34:59 -0500

    updated display and test data
    updated the display and used hardcoded test data instead of backend requests

--- front-end-web/src/pages/planner.js
+++ front-end-web/src/pages/planner.js
@@ -1,7 +1,5 @@
 
 import React from "react";
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { BiSolidPlane } from "react-icons/bi";
 import { BiSolidBed } from "react-icons/bi";
@@ -13,76 +11,76 @@ import { BsPencilFill } from "react-icons/bs";
 import { BsTrashFill } from "react-icons/bs";
 import { BsChatFill } from "react-icons/bs";
 
-// const planner = 
-//     {
-//         "plannerId": "5",
-//         "createdBy": "user123",
-//         "startDate": "2023-10-01T00:00:00Z",
-//         "endDate": "2023-10-10T00:00:00Z",
-//         "name": "Trip to Spain",
-//         "description": "Exploring Barcelona and Madrid",
-//         "roUsers": ["user456"],
-//         "rwUsers": ["user789"],
-//         "destinations": ["dest001"],
-//         "transportations": ["trans001"]
-//       };
+const planner = 
+    {
+        "plannerId": "5",
+        "createdBy": "user123",
+        "startDate": "2023-10-01T00:00:00Z",
+        "endDate": "2023-10-10T00:00:00Z",
+        "name": "Trip to Spain",
+        "description": "Exploring Barcelona and Madrid",
+        "roUsers": ["user456"],
+        "rwUsers": ["user789"],
+        "destinations": ["dest001"],
+        "transportations": ["trans001"]
+      };
 
-// const destinations = [
-//     {
-//         "destinationId": "dest001",
-//         "plannerId": "planner001",
-//         "name": "Madrid",
-//         "startDate": "2023-10-01",
-//         "endDate": "2023-10-05",
-//         "activities": ["activity001"],
-//         "accommodations": ["accom001"]
-//       }
-// ];
+const destinations = [
+    {
+        "destinationId": "dest001",
+        "plannerId": "planner001",
+        "name": "Madrid",
+        "startDate": "2023-10-01",
+        "endDate": "2023-10-05",
+        "activities": ["activity001"],
+        "accommodations": ["accom001"]
+      }
+];
 
-// const transportation = [
-//     {
-//         "transportationId": "trans001",
-//         "plannerId": "planner001",
-//         "type": "Flight",
-//         "details": "Flight from NYC to Madrid",
-//         "departureTime": "2023-10-01T08:00:00Z",
-//         "arrivalTime": "2023-10-01T20:00:00Z"
-//       }
-// ];
+const transportation = [
+    {
+        "transportationId": "trans001",
+        "plannerId": "planner001",
+        "type": "Flight",
+        "details": "Flight from NYC to Madrid",
+        "departureTime": "2023-10-01T08:00:00Z",
+        "arrivalTime": "2023-10-01T20:00:00Z"
+      }
+];
 
-// const accommodations = [
-//     {
-//         "accommodationId": "accom001",
-//         "destinationId": "dest001",
-//         "name": "Madrid Hotel",
-//         "address": "123 Main St, Madrid",
-//         "checkInDate": "2023-10-01",
-//         "checkOutDate": "2023-10-05"
-//       }
-// ];
+const accommodations = [
+    {
+        "accommodationId": "accom001",
+        "destinationId": "dest001",
+        "name": "Madrid Hotel",
+        "address": "123 Main St, Madrid",
+        "checkInDate": "2023-10-01",
+        "checkOutDate": "2023-10-05"
+      }
+];
 
-// const activities = [
-//     {
-//         "activityId": "activity001",
-//         "destinationId": "dest001",
-//         "name": "Visit Prado Museum",
-//         "date": "2023-10-02",
-//         "time": "10:00",
-//         "locations": ["loc001"],
-//         "votes": ["vote001"],
-//         "comments": ["comment001"]
-//       }
-// ];
+const activities = [
+    {
+        "activityId": "activity001",
+        "destinationId": "dest001",
+        "name": "Visit Prado Museum",
+        "date": "2023-10-02",
+        "time": "10:00",
+        "locations": ["loc001"],
+        "votes": ["vote001"],
+        "comments": ["comment001"]
+      }
+];
 
-// const locations = [
-//     {
-//         "locationId": "loc001",
-//         "activityId": "activity001",
-//         "createdBy": "user123",
-//         "name": "Prado Museum",
-//         "address": "C. de Ruiz de Alarcón, 23, 28014 Madrid"
-//       }
-// ];
+const locations = [
+    {
+        "locationId": "loc001",
+        "activityId": "activity001",
+        "createdBy": "user123",
+        "name": "Prado Museum",
+        "address": "C. de Ruiz de Alarcón, 23, 28014 Madrid"
+      }
+];
 
 // const votes = [
 //     {
@@ -93,106 +91,18 @@ import { BsChatFill } from "react-icons/bs";
 //       }
 // ];
 
-// const comments = [
-//     {
-//         "commentId": "comment001",
-//         "activityId": "activity001",
-//         "createdBy": "user456",
-//         "content": "Can't wait to visit!"
-//       }
-// ];
+const comments = [
+    {
+        "commentId": "comment001",
+        "activityId": "activity001",
+        "createdBy": "user456",
+        "content": "Can't wait to visit!"
+      }
+];
 
 function Planner() {
     const { plannerId, access } = useParams();
     const isReadOnly = access === "ro";
-
-    const [planner, setPlanner] = useState([]);
-    const [destinations, setDestinations] = useState([]);
-    const [transportation, setTransportation] = useState([]);
-    const [accommodations, setAccommodations] = useState([]);
-    const [activities, setActivities] = useState([]);
-
-    const [locations, setLocations] = useState([]);
-    const [comments, setComments] = useState([]);
-    // const [votes, setVotes] = useState([]);
-
-    useEffect(() => {
-        axios
-        .get('http://localhost:8080/planner/'+plannerId)
-        .then((response) => setPlanner(response.data))
-        .catch((error) => console.error('Error fetching planner:', error));
-    }, []);
-
-    useEffect(() => {
-        axios
-        .get('http://localhost:8080/planner/'+plannerId+'/destination')
-        .then((response) => setDestinations(response.data))
-        .catch((error) => console.error('Error fetching destination:', error));
-    }, []);
-
-    useEffect(() => {
-        axios
-        .get('http://localhost:8080/planner/'+plannerId+'/transportation')
-        .then((response) => setTransportation(response.data))
-        .catch((error) => console.error('Error fetching transportation:', error));
-    }, []);
-
-    useEffect(() => {
-        axios
-        .get('http://localhost:8080/planner/'+plannerId+'/destination/'+destinations[0].destinationId+'/accommodation')
-        .then((response) => setAccommodations(response.data))
-        .catch((error) => console.error('Error fetching accommodations:', error));
-    }, []);
-
-    useEffect(() => {
-        axios
-        .get('http://localhost:8080/planner/'+plannerId+'/destination/'+destinations[0].destinationId+'/activity')
-        .then((response) => setActivities(response.data))
-        .catch((error) => console.error('Error fetching activities:', error));
-    }, []);
-
-    useEffect(() => {
-        axios
-        .get('http://localhost:8080/planner/'+plannerId+'/destination/'+destinations[0].destinationId+'/comment')
-        .then((response) => setComments(response.data))
-        .catch((error) => console.error('Error fetching comments:', error));
-    }, []);
-
-    // Fetch locations for each activity
-    useEffect(() => {
-        // For each activity, fetch locations by its id
-        activities.forEach(activity => {
-        axios.get('http://localhost:8080/planner/'+plannerId+'/destination/'+destinations[0].destinationId+'/activity/'+activity.activityId+'/location')
-            .then((response) => {
-            // Set locations in the state, with activity.id as the key
-            setLocations(prevLocations => ({
-                ...prevLocations,
-                [activity.activityId]: response.data, // Assuming data is a list of locations
-            }));
-            })
-            .catch((error) => {
-            console.error('Error fetching locations for activity ${activity.activityId}:', error);
-            });
-        });
-    }, [activities]);
-
-    // // Fetch votes for each activity
-    // useEffect(() => {
-    //     // For each activity, fetch votes by its id
-    //     activities.forEach(activity => {
-    //     axios.get('http://localhost:8080/planner/'+plannerId+'/destination/'+destinations[0].destinationId+'/activity/'+activity.activityId+'/vote')
-    //         .then((response) => {
-    //         // Set votes in the state, with activity.id as the key
-    //         setVotes(prevVotes => ({
-    //             ...prevVotes,
-    //             [activity.activityId]: response.data, // Assuming data is a list of votes
-    //         }));
-    //         })
-    //         .catch((error) => {
-    //         console.error('Error fetching locations for activity ${activity.activityId}:', error);
-    //         });
-    //     });
-    // }, [activities]);
 
     return (
         <div className="Page-color">
@@ -219,8 +129,8 @@ function Planner() {
                     <div className="List-item" key={transport.transportationId}>
                         {!isReadOnly &&(
                             <div className="Right-side">
-                                <button className="Icon-button"> <BsPencilFill /></button> 
-                                <button className="Icon-button"> <BsTrashFill /></button>
+                                <button className="Icon-button" disabled> <BsPencilFill /></button> 
+                                <button className="Icon-button" disabled> <BsTrashFill /></button>
                             </div>
                         )}
                         {transport.type}
@@ -231,7 +141,7 @@ function Planner() {
                 ))}
                 {!isReadOnly &&(
                     <div>
-                        <button className="Icon-button"><BsFillPlusCircleFill /></button >
+                        <button className="Icon-button" disabled><BsFillPlusCircleFill /></button >
                     </div>
                 )}
                 <p/>
@@ -243,8 +153,8 @@ function Planner() {
                     <div className="List-item" key={accomodation.accommodationId}>
                         {!isReadOnly &&(
                             <div className="Right-side">
-                                <button className="Icon-button"> <BsPencilFill /></button> 
-                                <button className="Icon-button"> <BsTrashFill /></button>
+                                <button className="Icon-button" disabled> <BsPencilFill /></button> 
+                                <button className="Icon-button" disabled> <BsTrashFill /></button>
                             </div>
                         )}
                         {accomodation.name}
@@ -255,7 +165,7 @@ function Planner() {
                 ))}
                 {!isReadOnly &&(
                     <div>
-                        <button className="Icon-button"><BsFillPlusCircleFill /></button >
+                        <button className="Icon-button" disabled><BsFillPlusCircleFill /></button >
                     </div>
                 )}
                 <p/>
@@ -267,8 +177,8 @@ function Planner() {
                     <div className="List-item" key={activity.activityId}>
                         {!isReadOnly &&(
                             <div className="Right-side">
-                                <button className="Icon-button"> <BsPencilFill /></button> 
-                                <button className="Icon-button"> <BsTrashFill /></button>
+                                <button className="Icon-button" disabled> <BsPencilFill /></button> 
+                                <button className="Icon-button" disabled> <BsTrashFill /></button>
                             </div>
                         )}
                         {activity.name}
@@ -277,22 +187,12 @@ function Planner() {
                         
                         <p/>
                         Locations:
-                        {/* {locations.map((location) => (
+                        {locations.map((location) => (
                             <div className="List-item"  key={location.locationId}>
                                 {location.name}
                                 <div className="Planner-item">{location.address}</div>
                             </div>
-                        ))} */}
-                        {locations[activity.activityId] ? (
-                            locations[activity.activityId].map(location => (
-                                <div className="List-item"  key={location.locationId}>
-                                    {location.name}
-                                    <div className="Planner-item">{location.address}</div>
-                                </div>
-                            ))
-                        ) : (
-                            <div className="List-item">Loading locations...</div>
-                        )}
+                        ))}
 
                         {/* <div className="Planner-vote">
                             Vote Score:
@@ -312,7 +212,7 @@ function Planner() {
 
                 {!isReadOnly &&(
                     <div>
-                        <button className="Icon-button"><BsFillPlusCircleFill /></button >
+                        <button className="Icon-button" disabled><BsFillPlusCircleFill /></button >
                     </div>
                 )}
                 
@@ -328,7 +228,7 @@ function Planner() {
                 ))}
                 {!isReadOnly &&(
                     <div>
-                        <button className="Icon-button"><BsFillPlusCircleFill /></button >
+                        <button className="Icon-button" disabled><BsFillPlusCircleFill /></button >
                     </div>
                 )}
 
--- front-end-web/src/pages/planners.js
+++ front-end-web/src/pages/planners.js
@@ -1,54 +1,41 @@
 
 import React from "react";
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import axios from 'axios';
 
-// const plannerList = [
-//     {
-//         "plannerId": "5",
-//         "createdBy": "user123",
-//         "startDate": "2023-10-01T00:00:00Z",
-//         "endDate": "2023-10-10T00:00:00Z",
-//         "name": "Trip to Spain",
-//         "description": "Exploring Barcelona and Madrid",
-//         "roUsers": ["user456"],
-//         "rwUsers": ["user789"],
-//         "destinations": ["dest001"],
-//         "transportations": ["trans001"]
-//       }
-// ];
+const editPlannerList = [
+    {
+        "plannerId": "5",
+        "createdBy": "user123",
+        "startDate": "2023-10-01T00:00:00Z",
+        "endDate": "2023-10-10T00:00:00Z",
+        "name": "Trip to Spain",
+        "description": "Exploring Barcelona and Madrid",
+        "roUsers": ["user456"],
+        "rwUsers": ["user789"],
+        "destinations": ["dest001"],
+        "transportations": ["trans001"]
+      }
+];
 
-const uid = 0; //temp userid
+const viewPlannerList = [
+    {
+        "plannerId": "5",
+        "createdBy": "user123",
+        "startDate": "2023-10-01T00:00:00Z",
+        "endDate": "2023-10-10T00:00:00Z",
+        "name": "Trip to Spain",
+        "description": "Exploring Barcelona and Madrid",
+        "roUsers": ["user456"],
+        "rwUsers": ["user789"],
+        "destinations": ["dest001"],
+        "transportations": ["trans001"]
+      }
+];
 
 function Planners() {
-    const [editPlannerList, setPlannersRW] = useState([]);
-    const [viewPlannerList, setPlannersRO] = useState([]);
-
-    const getPlanners = async (e) => {
-        e.preventDefault();
-    
-        try {
-            await axios
-            .get(`http://localhost:8080/planner`, {
-                params: { userId: uid, access:'rw'}
-            })
-            .then((response) => setPlannersRW(response.data));
-
-            await axios
-            .get(`http://localhost:8080/planner`, {
-                params: { userId: uid, access:'ro'}
-            })
-            .then((response) => setPlannersRO(response.data));
-            
-        } catch (err) {
-            console.error(err);
-        }
-    };
 
     return (
         <div>
-            {getPlanners}
             <header className="Page-header">
                 <p>
                     Your Planners:
