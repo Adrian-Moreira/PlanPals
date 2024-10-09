d71ef45cbee3151f4c96669091a560ff563de57b
JoshuaWolowich <120571945+JoshuaWolowich@users.noreply.github.com>
2024-10-09 14:37:03 -0500

    Reapply "Updated planner display and used backend for data"
    This reverts commit 07b7259e6156d0c3e02234175b8876aa587252c7.

--- front-end-web/src/pages/planners.js
+++ front-end-web/src/pages/planners.js
@@ -1,25 +1,54 @@
 
 import React from "react";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
-const plannerList = [
-    {
-        "plannerId": "5",
-        "createdBy": "user123",
-        "startDate": "2023-10-01T00:00:00Z",
-        "endDate": "2023-10-10T00:00:00Z",
-        "name": "Trip to Spain",
-        "description": "Exploring Barcelona and Madrid",
-        "roUsers": ["user456"],
-        "rwUsers": ["user789"],
-        "destinations": ["dest001"],
-        "transportations": ["trans001"]
-      }
-];
+// const plannerList = [
+//     {
+//         "plannerId": "5",
+//         "createdBy": "user123",
+//         "startDate": "2023-10-01T00:00:00Z",
+//         "endDate": "2023-10-10T00:00:00Z",
+//         "name": "Trip to Spain",
+//         "description": "Exploring Barcelona and Madrid",
+//         "roUsers": ["user456"],
+//         "rwUsers": ["user789"],
+//         "destinations": ["dest001"],
+//         "transportations": ["trans001"]
+//       }
+// ];
+
+const uid = 0; //temp userid
 
 function Planners() {
+    const [editPlannerList, setPlannersRW] = useState([]);
+    const [viewPlannerList, setPlannersRO] = useState([]);
+
+    const getPlanners = async (e) => {
+        e.preventDefault();
+    
+        try {
+            await axios
+            .get(`http://localhost:8080/planner`, {
+                params: { userId: uid, access:'rw'}
+            })
+            .then((response) => setPlannersRW(response.data));
+
+            await axios
+            .get(`http://localhost:8080/planner`, {
+                params: { userId: uid, access:'ro'}
+            })
+            .then((response) => setPlannersRO(response.data));
+            
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
         <div>
+            {getPlanners}
             <header className="Page-header">
                 <p>
                     Your Planners:
@@ -28,9 +57,9 @@ function Planners() {
             </header>
 
             <div className="List">
-                {plannerList.map((planner) => (
+                {editPlannerList.map((planner) => (
                     <div key={planner.plannerId}>
-                        <Link className="List-button" to={`/planner/${planner.plannerId}`}>{planner.name}</Link>
+                        <Link className="List-button" to={`/planner/rw/${planner.plannerId}`}>{planner.name}</Link>
                     </div>
                 ))}
             </div>
@@ -43,9 +72,9 @@ function Planners() {
             </header>
 
             <div className="List">
-                {plannerList.map((planner) => (
-                    <div key={planner.id}>
-                        <Link className="List-button" to={`/planner/${planner.id}`}>{planner.name}</Link>
+                {viewPlannerList.map((planner) => (
+                    <div key={planner.plannerId}>
+                        <Link className="List-button" to={`/planner/ro/${planner.plannerId}`}>{planner.name}</Link>
                     </div>
                 ))}
             </div>
