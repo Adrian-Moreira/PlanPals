4ca13452d8ce540275a64c92f8e2ba1c7c66e885
h-kt <lammsuen@icloud.com>
2024-11-08 09:29:21 -0600

    Misc

--- backend/planner-service/src/app.ts
+++ backend/planner-service/src/app.ts
@@ -3,10 +3,9 @@
 import path from 'node:path'
 import { inspect } from 'node:util'
 import { createServer, Server } from 'node:http'
-import express, { Express, Request, Response } from 'express'
+import express, { Express, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import { StatusCodes } from 'http-status-codes'
-import { rateLimit } from 'express-rate-limit'
 import { Mongoose } from 'mongoose'
 
 import config from './config'
@@ -18,19 +17,12 @@ const port: number = config.server.port ? parseInt(config.server.port) : 8080
 export let MongooseConnection: Mongoose
 export let PPAPP: PlanPals
 
-// const rateLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   limit: 100,
-//   standardHeaders: 'draft-7',
-//   legacyHeaders: false,
-// })
-
 const healthChecker = (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('OK')
 }
 
 const landingPage = (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.status(StatusCodes.OK).sendFile(path.join(__dirname, 'public', 'index.html'))
 }
 
 type PlanPals = {
@@ -39,14 +31,50 @@ type PlanPals = {
   db: Mongoose
 }
 
+import { writeFileSync } from 'fs'
+import { performance, PerformanceObserver } from 'perf_hooks'
+import * as prof from 'v8-profiler-next'
+
+// Setup performance metrics observer
+const obs = new PerformanceObserver((list) => {
+  const entries = list.getEntries()
+  entries.forEach((entry) => {
+    console.log(`${entry.name}: ${entry.duration}ms`)
+  })
+})
+obs.observe({ entryTypes: ['measure'], buffered: true })
+
+// Middleware to measure route execution time
+const measureRouteTime = (req: Request, res: Response, next: NextFunction) => {
+  const routeName = `${req.method} ${req.path}`
+  performance.mark(`${routeName}-start`)
+
+  res.on('finish', () => {
+    performance.mark(`${routeName}-halt`)
+    performance.measure(routeName, `${routeName}-start`, `${routeName}-halt`)
+  })
+
+  next()
+}
+
 export function initExpress(app: Express): Express {
   app.use(express.json())
   app.use(cors())
   app.use(express.urlencoded({ extended: false }))
   app.use(express.static('public'))
   // app.use(rateLimiter)
+  app.use(measureRouteTime)
   app.get('/', landingPage)
   app.get('/health', healthChecker)
+  app.get('/profile-pp-start', (req, res) => {
+    prof.startProfiling('CPU profile')
+    res.send('CPU profiling started')
+  })
+  app.get('/profile-pp-halt', (req, res) => {
+    const profile = prof.stopProfiling('CPU profile')
+    writeFileSync('./pp-profile.cpuprofile', JSON.stringify(profile))
+    res.send('CPU profile saved to pp-profile.cpuprofile')
+  })
   app.use(router)
   app.use(RequestUtils.mkErrorResponse)
   return app
