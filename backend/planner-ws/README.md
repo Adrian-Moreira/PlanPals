3cb269b63f363b9a4a3115f30425678f84521c60
h-kt <lammsuen@icloud.com>
2024-11-15 22:30:02 -0600

    Listener update

--- backend/planner-ws/README.md
+++ backend/planner-ws/README.md
@@ -14,8 +14,12 @@ Before starting, ensure you have Deno 2.0 installed on your system. If not, you 
    ```bash
    deno run dev
    ```
+   #### Alternatively
+   ```bash
+   deno run --allow-net --allow-read --allow-env --watch src/main.ts --port ${PORT_NUMBER} --log-level ${LOG_LEVEL}
+  ```
 
-   ### Step 3: Build for Production
+   ### Step 3: Build Executable
    To build the app for production, run:
    ```bash
    deno run build
--- backend/planner-ws/src/main.ts
+++ backend/planner-ws/src/main.ts
@@ -1,10 +1,8 @@
 import { parseArgs } from "jsr:@std/cli/parse-args";
-import { difference } from "@std/datetime";
 import * as amqp from "npm:amqplib";
 import { connectToRabbitMQ, initRabbitChannel } from "./network/rabbit.ts";
 import { processFlags, processLoglevel } from "./utils/args.ts";
 import { startListening } from "./network/websocket.ts";
-import { PrintInfo } from "./utils/info.ts";
 import { broadcastPendingMessages } from "./network/broadcast.ts";
 import {
   cleanUpClients,
@@ -12,13 +10,11 @@ import {
   clientSubsByTopic,
 } from "./data/clients.ts";
 import { queues } from "./data/queue.ts";
-import { mkTopic } from "./data/topic.ts";
-
-interface ServinsArgs {
+import { setUpSignalListeners } from "./utils/signalListeners.ts";
+export interface ServinsArgs {
   updateInt: number;
   cleanUpInt: number;
 }
-
 export async function StartServing(
   port: number = 8000,
   args: ServinsArgs = { updateInt: 1000, cleanUpInt: 120000 },
@@ -29,9 +25,7 @@ export async function StartServing(
       "amqp://user:password@localhost:5672",
   );
   await initRabbitChannel(rabbitChannel);
-
   const wsServer = startListening({ port, ac });
-
   const _broadcastTicker = setInterval(
     broadcastPendingMessages,
     args.updateInt,
@@ -42,56 +36,16 @@ export async function StartServing(
   );
   return { ac, rabbitChannel, wsServer, startDate: new Date() };
 }
-
 export interface ServerAttrArgs {
   rabbitChannel: amqp.Channel;
   wsServer: Deno.HttpServer;
   startDate: Date;
 }
-
 export async function StopServing(args: ServerAttrArgs) {
   await args.rabbitChannel.close();
   await args.wsServer.shutdown();
   console.error("Server closed.");
 }
-
-function setUpSignalListeners(port: number, args: ServerAttrArgs) {
-  Deno.addSignalListener("SIGINT", async () => {
-    console.error("\nReceived SIGINT, stopping...");
-    await StopServing(args);
-    Deno.exit();
-  });
-  if (Deno.build.os === "linux" || Deno.build.os === "darwin") {
-    Deno.addSignalListener("SIGTERM", async () => {
-      console.error("\nReceived SIGTERM, stopping...");
-      await StopServing(args);
-      Deno.exit();
-    });
-
-    Deno.addSignalListener("SIGHUP", async () => {
-      console.error("\nReceived SIGHUP, restarting...");
-      await StopServing(args);
-      queues.forEach((q) => q.pending.length = 0);
-      clientSockets.clear();
-      clientSubsByTopic.clear();
-      setTimeout(async () => {
-        const newAttr = await StartServing(port);
-        args.rabbitChannel = newAttr.rabbitChannel;
-        args.wsServer = newAttr.wsServer;
-        PrintInfo();
-      }, 1000);
-    });
-
-    Deno.addSignalListener("SIGUSR1", () => {
-      const upTime = difference(args.startDate, new Date());
-      console.error(
-        `Started at: ${args.startDate}; Uptime: ${upTime.seconds} secs`,
-      );
-      PrintInfo();
-    });
-  }
-}
-
 if (import.meta.main) {
   const { logLevel, port } = processFlags(parseArgs(Deno.args, {
     string: ["log-level", "port"],
@@ -101,16 +55,19 @@ if (import.meta.main) {
     },
   }));
   processLoglevel({ logLevel });
-
   const start = async () => {
     const serverAttrs = await StartServing(port);
-    setUpSignalListeners(port, serverAttrs);
+    setUpSignalListeners({
+      StartServing,
+      StopServing,
+      queues,
+      clientSockets,
+      clientSubsByTopic,
+    })(port, serverAttrs);
   };
-
   const createRetryTicker = () => {
     let attempts = 0;
     const maxAttempts = 5;
-
     const ticker = setInterval(async () => {
       if (attempts >= maxAttempts) {
         console.error("Max retry attempts reached. Exiting...");
@@ -120,10 +77,8 @@ if (import.meta.main) {
       attempts++;
       await startWithRetry(ticker)();
     }, 5000);
-
     return ticker;
   };
-
   const startWithRetry = (ticker: number) => async () => {
     try {
       await start();
@@ -132,6 +87,5 @@ if (import.meta.main) {
       console.error("Failed to start server:", e);
     }
   };
-
   await startWithRetry(createRetryTicker())();
 }
