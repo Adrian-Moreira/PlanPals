6265b74f2b23355de32a8255fb177521728724f3
h-kt <lammsuen@icloud.com>
2024-11-16 21:36:51 -0600

    Updated formatting and README

--- backend/planner-ws/src/network/broadcast.ts
+++ backend/planner-ws/src/network/broadcast.ts
@@ -5,57 +5,56 @@ import { mkTopic, Topic } from "../data/topic.ts";
 export type MessageActionType = "update" | "delete";
 
 interface PublishUpdateArgs {
-	topic: Topic;
-	action: MessageActionType | string;
-	message: PPObject;
+    topic: Topic;
+    action: MessageActionType | string;
+    message: PPObject;
 }
 
-const sendUpdateToClient =
-	(args: PublishUpdateArgs) => (socket?: WebSocket) => {
-		if (socket && socket.readyState === WebSocket.OPEN) {
-			socket.send(JSON.stringify(args));
-		}
-	};
+const sendUpdateToClient = (socket: WebSocket, args: PublishUpdateArgs) => {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify(args));
+    }
+};
 
 export const publishMessage = (
-	qItem: QueueItem,
-	qName: string,
+    qItem: QueueItem,
+    qName: string,
 ) => {
-	clientSubsByTopic.forEach((clients, topic) => {
-		const topicObject = mkTopic(topic);
-		if (!topicObject) return;
+    clientSubsByTopic.forEach((clients, topic) => {
+        const topicObject = mkTopic(topic);
+        if (!topicObject) return;
 
-		const userIdMatch = qItem.object.userIds?.some((uid) =>
-			uid === topicObject.id
-		);
-		const plannerIdMatch = qItem.object.plannerId === topicObject.id;
+        const userIdMatch = qItem.object.userIds?.some((uid) =>
+            uid === topicObject.id
+        );
+        const plannerIdMatch = qItem.object.plannerId === topicObject.id;
 
-		const plannersTopic = topicObject.type === "planners" && userIdMatch &&
-			qItem.object.type === "Planner";
-		const plannerTopic = topicObject.type === "planner" && plannerIdMatch;
-		const inboxTopic = topicObject.type === "inbox" && userIdMatch;
+        const plannersTopic = topicObject.type === "planners" && userIdMatch &&
+            qItem.object.type === "Planner";
+        const plannerTopic = topicObject.type === "planner" && plannerIdMatch;
+        const inboxTopic = topicObject.type === "inbox" && userIdMatch;
 
-		const validTopic = plannersTopic || plannerTopic || inboxTopic;
+        const validTopic = plannersTopic || plannerTopic || inboxTopic;
 
-		if (!validTopic) return;
-
-		const send = sendUpdateToClient({
-			topic: topicObject,
-			action: qName,
-			message: qItem.object,
-		});
-		clients.values().map(([uuid]) => clientSockets.get(uuid)).forEach(send);
-	});
+        if (!validTopic) return;
+        clients.forEach((uuid) => {
+            sendUpdateToClient(clientSockets.get(uuid)!, {
+                topic: topicObject,
+                action: qName,
+                message: qItem.object,
+            });
+        });
+    });
 };
 
 export const broadcastPendingMessages = () => {
-	queues.forEach((q) => {
-		if (!(q.pending.length > 0)) return;
+    queues.forEach((q) => {
+        if (!(q.pending.length > 0)) return;
 
-		console.info("PublishingMessage: ", q.name, Deno.inspect(q.pending));
-		q.pending.forEach((update) => {
-			publishMessage(update, q.name);
-		});
-		q.pending.length = 0;
-	});
+        console.info("PublishingMessage: ", q.name, Deno.inspect(q.pending));
+        q.pending.forEach((update) => {
+            publishMessage(update, q.name);
+        });
+        q.pending.length = 0;
+    });
 };
