1610287142b5e6b4ea8c6939d5784590eabd80a5
h-kt <lammsuen@icloud.com>
2024-11-16 21:23:37 -0600

    Updated formatting and README

--- backend/planner-ws/src/main.ts
+++ backend/planner-ws/src/main.ts
@@ -5,89 +5,89 @@ import { processFlags, processLoglevel } from "./utils/args.ts";
 import { startListening } from "./network/websocket.ts";
 import { broadcastPendingMessages } from "./network/broadcast.ts";
 import {
-    cleanUpClients,
-    clientSockets,
-    clientSubsByTopic,
+	cleanUpClients,
+	clientSockets,
+	clientSubsByTopic,
 } from "./data/clients.ts";
 import { queues } from "./data/queue.ts";
 
 import { setUpSignalListeners } from "./utils/signalListeners.ts";
 export interface ServinsArgs {
-    updateInt: number;
-    cleanUpInt: number;
+	updateInt: number;
+	cleanUpInt: number;
 }
 export async function StartServing(
-    port: number = 8000,
-    args: ServinsArgs = { updateInt: 1000, cleanUpInt: 120000 },
+	port: number = 8000,
+	args: ServinsArgs = { updateInt: 1000, cleanUpInt: 120000 },
 ) {
-    const ac = new AbortController();
-    const rabbitChannel = await connectToRabbitMQ(
-        Deno.env.get("RABBITMQ_CONNECTIONSTRING") ??
-            "amqp://user:password@localhost:5672",
-    );
-    await initRabbitChannel(rabbitChannel);
-    const wsServer = startListening({ port, ac });
-    const _broadcastTicker = setInterval(
-        broadcastPendingMessages,
-        args.updateInt,
-    );
-    const _cleanUpClientsTicker = setInterval(
-        cleanUpClients,
-        args.cleanUpInt,
-    );
-    return { ac, rabbitChannel, wsServer, startDate: new Date() };
+	const ac = new AbortController();
+	const rabbitChannel = await connectToRabbitMQ(
+		Deno.env.get("RABBITMQ_CONNECTIONSTRING") ??
+			"amqp://user:password@localhost:5672",
+	);
+	await initRabbitChannel(rabbitChannel);
+	const wsServer = startListening({ port, ac });
+	const _broadcastTicker = setInterval(
+		broadcastPendingMessages,
+		args.updateInt,
+	);
+	const _cleanUpClientsTicker = setInterval(
+		cleanUpClients,
+		args.cleanUpInt,
+	);
+	return { ac, rabbitChannel, wsServer, startDate: new Date() };
 }
 export interface ServerAttrArgs {
-    rabbitChannel: amqp.Channel;
-    wsServer: Deno.HttpServer;
-    startDate: Date;
+	rabbitChannel: amqp.Channel;
+	wsServer: Deno.HttpServer;
+	startDate: Date;
 }
 export async function StopServing(args: ServerAttrArgs) {
-    await args.rabbitChannel.close();
-    await args.wsServer.shutdown();
-    console.error("Server closed.");
+	await args.rabbitChannel.close();
+	await args.wsServer.shutdown();
+	console.error("Server closed.");
 }
 
 if (import.meta.main) {
-    const { logLevel, port } = processFlags(parseArgs(Deno.args, {
-        string: ["log-level", "port"],
-        default: {
-            "log-level": "3",
-            "port": "8000",
-        },
-    }));
-    processLoglevel({ logLevel });
-    const start = async () => {
-        const serverAttrs = await StartServing(port);
-        setUpSignalListeners({
-            StartServing,
-            StopServing,
-            queues,
-            clientSockets,
-            clientSubsByTopic,
-        })(port, serverAttrs);
-    };
-    const createRetryTicker = () => {
-        let attempts = 0;
-        const maxAttempts = 5;
-        const ticker = setInterval(async () => {
-            if (attempts >= maxAttempts) {
-                console.error("Max retry attempts reached. Exiting...");
-                clearInterval(ticker);
-                Deno.exit(1);
-            }
-            attempts++;
-            await startWithRetry(ticker)();
-        }, 5000);
-        return ticker;
-    };
-    const startWithRetry = (ticker: number) => async () => {
-        try {
-            await start();
-            clearInterval(ticker);
-        } catch (e) {
-            console.error("Failed to start server:", e);
-        }
-    };
-    await startWithRetry(createRetryTicker())();
+	const { logLevel, port } = processFlags(parseArgs(Deno.args, {
+		string: ["log-level", "port"],
+		default: {
+			"log-level": "3",
+			"port": "8000",
+		},
+	}));
+	processLoglevel({ logLevel });
+	const start = async () => {
+		const serverAttrs = await StartServing(port);
+		setUpSignalListeners({
+			StartServing,
+			StopServing,
+			queues,
+			clientSockets,
+			clientSubsByTopic,
+		})(port, serverAttrs);
+	};
+	const createRetryTicker = () => {
+		let attempts = 0;
+		const maxAttempts = 5;
+		const ticker = setInterval(async () => {
+			if (attempts >= maxAttempts) {
+				console.error("Max retry attempts reached. Exiting...");
+				clearInterval(ticker);
+				Deno.exit(1);
+			}
+			attempts++;
+			await startWithRetry(ticker)();
+		}, 5000);
+		return ticker;
+	};
+	const startWithRetry = (ticker: number) => async () => {
+		try {
+			await start();
+			clearInterval(ticker);
+		} catch (e) {
+			console.error("Failed to start server:", e);
+		}
+	};
+	await startWithRetry(createRetryTicker())();
 }
--- backend/planner-ws/src/network/rabbit.ts
+++ backend/planner-ws/src/network/rabbit.ts
@@ -4,47 +4,47 @@ import { IQueue, PPObject, queues } from "../data/queue.ts";
 let rabbitChannel: amqp.Channel;
 
 const mkQueueConsumer =
-    (ch: amqp.Channel) => (queue: IQueue) => (msg: amqp.ConsumeMessage) => {
-        console.debug("Received Message!");
-        if (!msg) return;
-        const ppObject = JSON.parse(msg.content.toString()) as PPObject;
-        console.log(`From Queue: ${queue.name} => `, ppObject);
-        queue.pending.push({ object: ppObject });
-        ch.ack(msg);
-    };
+	(ch: amqp.Channel) => (queue: IQueue) => (msg: amqp.ConsumeMessage) => {
+		console.debug("Received Message!");
+		if (!msg) return;
+		const ppObject = JSON.parse(msg.content.toString()) as PPObject;
+		console.log(`From Queue: ${queue.name} => `, ppObject);
+		queue.pending.push({ object: ppObject });
+		ch.ack(msg);
+	};
 
 const initQueue = async (
-    q: IQueue,
-    ch: amqp.Channel,
-    consumer: (msg: amqp.ConsumeMessage) => void,
+	q: IQueue,
+	ch: amqp.Channel,
+	consumer: (msg: amqp.ConsumeMessage) => void,
 ) => {
-    console.debug("Initializing queue", q);
-    await ch.assertQueue(q.name, { durable: false });
-    ch.consume(q.name, consumer);
+	console.debug("Initializing queue", q);
+	await ch.assertQueue(q.name, { durable: false });
+	ch.consume(q.name, consumer);
 };
 
 const initQueues = async (qs: IQueue[], ch: amqp.Channel) => {
-    const mkConsumer = mkQueueConsumer(ch);
-    await Promise.all(qs.map(async (q) => {
-        await initQueue(q, ch, mkConsumer(q));
-    }));
+	const mkConsumer = mkQueueConsumer(ch);
+	await Promise.all(qs.map(async (q) => {
+		await initQueue(q, ch, mkConsumer(q));
+	}));
 };
 
 export const connectToRabbitMQ = async (str: string): amqp.Channel => {
-    const connection = await amqp.connect(str);
-    rabbitChannel = await connection.createChannel();
-    return rabbitChannel;
+	const connection = await amqp.connect(str);
+	rabbitChannel = await connection.createChannel();
+	return rabbitChannel;
 };
 
 export const initRabbitChannel = async (
-    ch: amqp.Channel,
+	ch: amqp.Channel,
 ): Promise<amqp.Channel> => {
-    if (ch) rabbitChannel = ch;
-    try {
-        await initQueues(queues, rabbitChannel);
-    } catch {
-        console.error("Failed while initilizing queues");
-        Deno.exit(1);
-    }
-    return rabbitChannel;
+	if (ch) rabbitChannel = ch;
+	try {
+		await initQueues(queues, rabbitChannel);
+	} catch {
+		console.error("Failed while initilizing queues");
+		Deno.exit(1);
+	}
+	return rabbitChannel;
 };
