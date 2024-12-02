6e77d12144a4996bd282a7251e602cd651ad2d35
Jay D. <123786050+JayDalool@users.noreply.github.com>
2024-12-02 07:05:34 -0600

    Added Notification for planners

--- frontend/web-frontend/src/components/Planners/PlannerCreateView.tsx
+++ frontend/web-frontend/src/components/Planners/PlannerCreateView.tsx
@@ -1,76 +1,85 @@
-import React, { useCallback, useEffect, useInsertionEffect, useState } from 'react'
-import * as MUI from '@mui/material/'
-import * as MUIcons from '@mui/icons-material'
-import dayjs from 'dayjs'
-import { useFormFields } from '../../lib/hooksLib'
-import DatePickerValue from '../Common/DatePickerValue'
-import TimePickerValue from '../Common/TimePickerValue'
-import { combineDateAndTime } from '../../lib/dateLib'
-import apiLib from '../../lib/apiLib'
-import { onError } from '../../lib/errorLib'
-import { useNavigate } from 'react-router-dom'
-import { useAtom } from 'jotai'
-import { PPUser, ppUserAtom } from '../../lib/authLib'
-import AdaptiveDialog from '../Common/AdaptiveDialog'
-import { userMapAtom } from '../../lib/appLib'
+import React, { useCallback, useEffect, useState } from 'react';
+import * as MUI from '@mui/material/';
+import * as MUIcons from '@mui/icons-material';
+import dayjs from 'dayjs';
+import { useFormFields } from '../../lib/hooksLib';
+import DatePickerValue from '../Common/DatePickerValue';
+import TimePickerValue from '../Common/TimePickerValue';
+import { combineDateAndTime } from '../../lib/dateLib';
+import apiLib from '../../lib/apiLib';
+import { onError } from '../../lib/errorLib';
+import { useNavigate } from 'react-router-dom';
+import { useAtom } from 'jotai';
+import { PPUser, ppUserAtom } from '../../lib/authLib';
+import AdaptiveDialog from '../Common/AdaptiveDialog';
+import { userMapAtom } from '../../lib/appLib';
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { useContext } from 'react';
+import { NotificationContext } from '../../components/Notifications/notificationContext';
 
 export interface PlannerCreateViewProps {
-  handelCancel: () => void
-  hasPlanner: boolean
-  setOnReload: React.Dispatch<React.SetStateAction<boolean>>
-  open: boolean
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+  handelCancel: () => void;
+  hasPlanner: boolean;
+  setOnReload: React.Dispatch<React.SetStateAction<boolean>>;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function PlannerCreateView(props: PlannerCreateViewProps) {
-  const nav = useNavigate()
+  const nav = useNavigate();
+  const theme = createTheme();
+  const { setNotification } = useContext(NotificationContext);
 
   const [fields, handleFieldChange] = useFormFields({
     plannerName: '',
     plannerDescription: '',
     destinationName: '',
-  })
-  const today = new Date()
-  const [startDate, setStartDate] = React.useState(dayjs(today))
-  const [endDate, setEndDate] = React.useState(dayjs(today))
-  const [startTime, setStartTime] = React.useState(dayjs(today))
-  const [endTime, setEndTime] = React.useState(dayjs(today))
-  const [palList, setPalList] = React.useState<PPUser[]>([])
-  const [selectedPals, setSelectedPals] = React.useState<PPUser[]>([])
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [userMap] = useAtom(userMapAtom)
-  const [pUser] = useAtom(ppUserAtom)
-  const [pError, setPError] = useState(false)
-  const [timeError, setTimeError] = useState(false)
+  });
+  const today = new Date();
+  const [startDate, setStartDate] = React.useState(dayjs(today));
+  const [endDate, setEndDate] = React.useState(dayjs(today));
+  const [startTime, setStartTime] = React.useState(dayjs(today));
+  const [endTime, setEndTime] = React.useState(dayjs(today));
+  const [palList, setPalList] = React.useState<PPUser[]>([]);
+  const [selectedPals, setSelectedPals] = React.useState<PPUser[]>([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [userMap] = useAtom(userMapAtom);
+  const [pUser] = useAtom(ppUserAtom);
+  const [pError, setPError] = useState(false);
+  const [timeError, setTimeError] = useState(false);
 
   const fetchPalList = useCallback(async () => {
     try {
-      const res = await apiLib.get('/user/ls', {})
-      const pals: PPUser[] = res.data.data
-      pals.map((p) => userMap.set(p._id, p))
-      setPalList(pals)
-      return res.data.success ? pals : []
+      const res = await apiLib.get('/user/ls', {});
+      const pals: PPUser[] = res.data.data;
+      pals.map((p) => userMap.set(p._id, p));
+      setPalList(pals);
+      return res.data.success ? pals : [];
     } catch {
-      console.error('No Users Fetched')
-      return []
+      console.error('No Users Fetched');
+      return [];
     }
-  }, [])
+  }, []);
 
   const validatePlannerForm = useCallback(() => {
-    const isNameValid = fields.plannerName.length > 0
-    setPError(!isNameValid)
-    const isTimeValid = startDate.isBefore(endDate) && combineDateAndTime(endDate, endTime).isBefore(new Date())
-    setTimeError(!isTimeValid)
-    return isNameValid && isTimeValid
-  }, [fields.plannerName, startDate, endDate, startTime, endTime])
+    const isNameValid = fields.plannerName.length > 0;
+    setPError(!isNameValid);
+    const isTimeValid =
+      startDate.isBefore(endDate) &&
+      combineDateAndTime(endDate, endTime).isBefore(new Date());
+    setTimeError(!isTimeValid);
+    return isNameValid && isTimeValid;
+  }, [fields.plannerName, startDate, endDate, startTime, endTime]);
 
   useEffect(() => {
-    fetchPalList()
-  }, [])
+    fetchPalList();
+  }, []);
 
   const handleSubmit = async (event: any) => {
-    event.preventDefault()
-    setIsLoading(true)
+    event.preventDefault();
+    setIsLoading(true);
     try {
       const res = await apiLib.post('/planner', {
         data: {
@@ -81,30 +90,20 @@ export default function PlannerCreateView(props: PlannerCreateViewProps) {
           endDate: combineDateAndTime(endDate, endTime).toISOString(),
           rwUsers: selectedPals.map((p) => p._id),
         },
-      })
-      if (res.data.success && fields?.destinationName !== '') {
-        const res2 = await apiLib.post(`/planner/${res.data.data._id}/destination`, {
-          data: {
-            createdBy: pUser.ppUser!._id,
-            startDate: combineDateAndTime(startDate, startTime).toISOString(),
-            endDate: combineDateAndTime(endDate, endTime).toISOString(),
-            name: fields.destinationName,
-          },
-        })
-        if (res2.data.success) {
-          props.setOnReload(true)
-          nav(`/planner/${res.data.data._id}`)
-        }
-      }
+      });
       if (res.data.success) {
-        props.setOnReload(true)
-        nav(`/planner/${res.data.data._id}`)
+        setNotification({ type: 'success', message: 'Planner created successfully!' });
+        props.setOnReload(true);
+        nav(`/planner/${res.data.data._id}`);
+      } else {
+        setNotification({ type: 'error', message: 'Failed to create planner. Please try again.' });
       }
-      setIsLoading(false)
+      setIsLoading(false);
     } catch (e) {
-      onError(e)
+      setNotification({ type: 'error', message: 'An error occurred while creating the planner.' });
+      onError(e);
     }
-  }
+  };
 
   const renderCreatePlanner = useCallback(() => {
     return (
@@ -138,13 +137,6 @@ export default function PlannerCreateView(props: PlannerCreateViewProps) {
           </MUI.Stack>
           <MUI.Box sx={{ flexDirection: 'column' }}>
             <MUI.Stack spacing={2}>
-              {/* <MUI.Typography variant="h6">Add a Destination</MUI.Typography>
-              <MUI.TextField
-                id="destinationName"
-                label="Destination"
-                value={fields.destinationName}
-                onChange={handleFieldChange}
-              /> */}
               <MUI.Typography variant="h6">Add Pals to plan together!</MUI.Typography>
               <MUI.Autocomplete
                 multiple
@@ -158,18 +150,18 @@ export default function PlannerCreateView(props: PlannerCreateViewProps) {
                 )}
                 renderTags={(tagValue, getTagProps) =>
                   tagValue.map((option, index) => {
-                    const { key, ...otherProps } = getTagProps({ index })
+                    const { key, ...otherProps } = getTagProps({ index });
                     return (
                       <MUI.Chip
-                        key={option._id} // Use explicit key
+                        key={option._id}
                         label={option.preferredName || option.userName}
                         {...otherProps}
                         onDelete={() => {
-                          const newSelected = selectedPals.filter((pal) => pal._id !== option._id)
-                          setSelectedPals(newSelected)
+                          const newSelected = selectedPals.filter((pal) => pal._id !== option._id);
+                          setSelectedPals(newSelected);
                         }}
                       />
-                    )
+                    );
                   })
                 }
               />
@@ -177,7 +169,7 @@ export default function PlannerCreateView(props: PlannerCreateViewProps) {
           </MUI.Box>
         </MUI.Box>
       </MUI.Box>
-    )
+    );
   }, [
     startDate,
     endDate,
@@ -192,24 +184,29 @@ export default function PlannerCreateView(props: PlannerCreateViewProps) {
     props.setOnReload,
     palList,
     selectedPals,
-  ])
+  ]);
 
   return (
-    <AdaptiveDialog
-      open={props.open}
-      setOpen={props.setOpen}
-      label={'CreateNewPlanner'}
-      title={'Creating a New Planner'}
-      children={renderCreatePlanner()}
-      cancelEnable={props.hasPlanner}
-      confirmEnable={true}
-      confirmButtonLabel="Save and Continue"
-      confirmIcon={<MUIcons.Save sx={{ mr: '0.5em' }} />}
-      cancelButtonLabel="Cancel"
-      cancelIcon={<MUIcons.Cancel sx={{ mr: '0.5em' }} />}
-      onConfirmHandler={(e) => {
-        handleSubmit(e)
-      }}
-    ></AdaptiveDialog>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <AdaptiveDialog
+          open={props.open}
+          setOpen={props.setOpen}
+          label={'CreateNewPlanner'}
+          title={'Creating a New Planner'}
+          children={renderCreatePlanner()}
+          cancelEnable={props.hasPlanner}
+          confirmEnable={true}
+          confirmButtonLabel="Save and Continue"
+          confirmIcon={<MUIcons.Save sx={{ mr: '0.5em' }} />}
+          cancelButtonLabel="Cancel"
+          cancelIcon={<MUIcons.Cancel sx={{ mr: '0.5em' }} />}
+          onConfirmHandler={(e) => {
+            handleSubmit(e);
+          }}
+        ></AdaptiveDialog>
+      </div>
+    </ThemeProvider>
+  );
 }
